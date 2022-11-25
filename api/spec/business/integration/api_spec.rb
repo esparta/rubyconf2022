6500e440fc23ac9101392459384dee300aa9cac3
Espartaco Palma <git@esparta.co>
2022-11-25 02:15:21 -0800

    Introducing web api as a Self Contain App
    It's barebones with a minimal scope of:
    
    - use the classes contained on the `Business::Schemas` module
    - expose an Web API

---
+++ api/spec/business/integration/api_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'rack/test'
+require 'spec_helper'
+require_relative '../../../business/api'
+
+OUTER_APP = Rack::Builder.parse_file('config.ru').first
+
+RSpec.describe Business::Api do
+  include Rack::Test::Methods
+
+  def app
+    OUTER_APP
+  end
+
+  let(:json) { JSON.parse(last_response.body) }
+
+  describe '/products/:id' do
+    let(:path) { "/v1/products/#{id}" }
+
+    context 'when using an UUID (Success' do
+      let(:id) { SecureRandom.uuid }
+
+      it do
+        get path
+
+        expect(last_response.status).to eq(200)
+        expect(json).to match('id' => id)
+      end
+    end
+
+    context 'when not using a UUID (Failure)' do
+      let(:id) { SecureRandom.random_number(100) }
+
+      it do
+        get path
+        expect(last_response.status).to eq(400)
+        expect(json).to match(
+          'reason' => {
+            'id' => [
+              'is not a valid UUID'
+            ]
+          }
+        )
+      end
+    end
+  end
+end
---
+++ api/spec/business/unit/api_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../../../business/api'
+
+RSpec.describe Business::Api do
+  subject(:api) do
+    described_class.new.call(request)
+  end
+
+  let(:request) do
+    {
+      'PATH_INFO' => path,
+      'REQUEST_METHOD' => request_method
+    }
+  end
+
+  let(:request_method) { 'GET' }
+
+  let(:content_type) do
+    { 'Content-Type' => 'application/json' }
+  end
+
+  describe '/v1/products/:id/' do
+    let(:path) { "/v1/products/#{id}" }
+
+    context 'when using an UUID (Success' do
+      let(:id) { SecureRandom.uuid }
+
+      it do
+        body = [{ id: id }.to_json]
+        expect(api).to match([200, content_type, body])
+      end
+    end
+
+    context 'when not using an UUID (Failure)' do
+      let(:id) { SecureRandom.random_number(100) }
+
+      it do
+        body = [{ reason: { id: ['is not a valid UUID'] } }.to_json]
+
+        expect(api).to match([400, content_type, body])
+      end
+    end
+  end
+end
