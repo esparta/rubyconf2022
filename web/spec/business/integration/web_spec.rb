1a1070a1865a324dcd8d195b578fff15252f4823
Espartaco Palma <git@esparta.co>
2022-11-25 17:47:15 -0800

    Add an spec for web/api/v1/products/:id
    The branch for api scope had no coverage, so I'm adding a
    basic one.
    
    Note that is not a complete test suite for the endpoint since
    we mean to have more comprenhensive ones inside the gem we
    are 'importing': `Business::Api`, meaning the maintainer
    should check the test suite located on the api folder.

--- web/spec/business/integration/web_spec.rb
+++ web/spec/business/integration/web_spec.rb
@@ -19,4 +19,24 @@ RSpec.describe Business::Web do
       expect(last_response.status).to eq(200)
     end
   end
+
+  describe '/api/v1/products/:id' do
+    let(:id) { SecureRandom.uuid }
+    let(:path) { "/api/v1/products/#{id}" }
+    let(:json) { JSON.parse(last_response.body) }
+
+    # Note we are using just one scenario, for a complete
+    # test suite review the `Business::Api` gem located on `api` folder
+    context 'when using an UUID (Success)' do
+      let(:id) { SecureRandom.uuid }
+
+      it do
+        get path
+
+        expect(last_response.status).to eq(200)
+        expect(last_response.content_type).to eq('application/json')
+        expect(json).to match('id' => id)
+      end
+    end
+  end
 end
